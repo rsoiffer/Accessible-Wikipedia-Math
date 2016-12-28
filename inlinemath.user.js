a1df22dfb6017fda3306e7b36167ad2a46e71b4a
Weirdbob95 <rory.soiffer@comcast.net>
2016-12-28 15:48:30 -0800

    Update inlinemath.user.js

--- inlinemath.user.js
+++ inlinemath.user.js
@@ -8,6 +8,7 @@
 // @icon         http://www.example.net/icon.png
 // ==/UserScript==
 
+
 NodeList.prototype.toArray = function () {
     return Array.prototype.slice.call(this);
 }
@@ -34,6 +35,11 @@ Node.prototype.stringify = function (level) {
 ///// [Templates]
 
 function GetTreeFromElement(element) {
+    console.log(element);
+    if (element.nodeName === "SPAN" && element.getAttribute("style") === "display:none") {
+        return new Node("row", "");
+    }
+
     if (CheckSqrtTemplate(element))
         return GetTreeFromSqrtElement(element);
     if (CheckRadicTemplate(element))
@@ -48,6 +54,12 @@ function GetTreeFromElement(element) {
         return GetTreeFromSubTemplate(element);
     if (CheckSupTemplate(element))
         return GetTreeFromSupTemplate(element);
+    if (CheckSuTemplate(element))
+        return GetTreeFromSuTemplate(element);
+    if (CheckOverTemplate(element))
+        return GetTreeFromOverTemplate(element);
+    if (CheckUnderTemplate(element))
+        return GetTreeFromUnderTemplate(element);
     if (CheckVariableTemplate(element))
         return GetTreeFromVariableTemplate(element);
     if (CheckDelimTemplate(element))
@@ -57,8 +69,13 @@ function GetTreeFromElement(element) {
         return CreateRowNode(element);
     }
 
+    var pseudoScripts = String.fromCharCode(0x0022, 0x0027, 0x002A, 0x0060, 0x00AA,
+            0x00B0, 0x00B2, 0x00B3, 0x00B4, 0x00B9, 0x00BA, 0x2018, 0x2019, 0x201A, 0x201B,
+            0x201D, 0x201E, 0x201F, 0x2032, 0x2033, 0x2034, 0x2035, 0x2036, 0x2037, 0x2057);
+
+
     // Parse text
-    var re = XRegExp("\\s*([0-9]+|\\pL+|[^\\s])\\s*");
+    var re = XRegExp("\\s*([0-9]+|\\pL+|[" + pseudoScripts + "]+|[^\\s])\\s*");
     var tokens = [];
     var m;
     var pos = 0;
@@ -68,10 +85,18 @@ function GetTreeFromElement(element) {
     }
 
     if (tokens.length === 1) {
-        return new Node("text", tokens[0]);
+        if (XRegExp.test(tokens[0], XRegExp("[" + pseudoScripts + "]+"))) {
+            return new Node("sup", "", [new Node("text", tokens[0])]);
+        } else {
+            return new Node("text", tokens[0]);
+        }
     } else {
         return new Node("row", "", tokens.map(function (token) {
-            return new Node("text", token);
+            if (XRegExp.test(token, XRegExp("[" + pseudoScripts + "]+"))) {
+                return new Node("sup", "", [new Node("text", token)]);
+            } else {
+                return new Node("text", token);
+            }
         }));
     }
 }
@@ -127,6 +152,23 @@ function CheckSupTemplate(element) {
     return element.nodeName === "SUP";
 }
 
+function CheckSuTemplate(element) {
+    return element.nodeName === "SPAN" && (element.getAttribute("style") ===
+            "display:inline-block;margin-bottom:-0.3em;vertical-align:-0.4em;line-height:1.2em;font-size:80%;text-align:left"
+            || element.getAttribute("style") ===
+            "display:inline-block;margin-bottom:-0.3em;vertical-align:-0.4em;line-height:1.2em;font-size:85%;text-align:right");
+}
+
+function CheckOverTemplate(element) {
+    return element.nodeName === "SPAN" && element.getAttribute("style") ===
+            "position:relative; margin-right:-0.75em; right:0.75em; bottom:0.75em;;";
+}
+
+function CheckUnderTemplate(element) {
+    return element.nodeName === "SPAN" && element.getAttribute("style") ===
+            "position:relative; margin-right:-0.75em; right:0.75em; top:0.45em;;";
+}
+
 function CheckVariableTemplate(element) {
     return element.nodeName === "I"
             && element.childNodes.length === 1
@@ -147,8 +189,13 @@ function GetTreeFromRadicElement(element) {
 }
 
 function GetTreeFromFractionTemplate(element) {
-    return new Node("frac", "", [GetTreeFromElement(element.childNodes[0]),
-        GetTreeFromElement(element.childNodes[2])]);
+    if (element.childNodes[1].childNodes[0].data === "/") {
+        return new Node("frac", "", [GetTreeFromElement(element.childNodes[0]),
+            GetTreeFromElement(element.childNodes[2])]);
+    } else {
+        return new Node("row", "", [GetTreeFromElement(element.childNodes[0]), new Node("text", "+"), new Node("frac", "", [
+                GetTreeFromElement(element.childNodes[2].childNodes[0]), GetTreeFromElement(element.childNodes[2].childNodes[2])])]);
+    }
 }
 
 function GetTreeFromFractionTemplate2(element) {
@@ -196,6 +243,18 @@ function GetTreeFromSubTemplate(element) {
 
 function GetTreeFromSupTemplate(element) {
     return new Node("sup", "", [CreateRowNode(element)]);
+}
+
+function GetTreeFromSuTemplate(element) {
+    return new Node("su", "", [GetTreeFromElement(element.childNodes[2]), GetTreeFromElement(element.childNodes[0])]);
+}
+
+function GetTreeFromOverTemplate(element) {
+    return new Node("sup", "2", [CreateRowNode(element)]);
+}
+
+function GetTreeFromUnderTemplate(element) {
+    return new Node("sub", "2", [CreateRowNode(element)]);
 }
 
 function GetTreeFromVariableTemplate(element) {
@@ -303,11 +362,20 @@ function FixSubSup(node) {
         if (node.children.length < 2) {
             node.type = "row";
         }
+    } else if (node.type === "su") {
+        if (node.children.length < 3) {
+            node.type = "row";
+        }
     } else {
         for (var i = 1; i < node.children.length; i++) {
             var child = node.children[i];
             if (child.type === "sub" || child.type === "sup") {
                 if (child.children.length === 1) {
+                    i--;
+                    child.children.unshift(node.children.splice(i, 1)[0]);
+                }
+            } else if (child.type === "su") {
+                if (child.children.length === 2) {
                     i--;
                     child.children.unshift(node.children.splice(i, 1)[0]);
                 }
@@ -336,9 +404,15 @@ function TreeToMathML(node) {
         case "integral":
             return "<msubsup><mo>" + node.value + "</mo>" + children + "</msubsup>"
         case "sub":
+            if (node.value === "2")
+                return "<munder>" + children + "</munder>";
             return "<msub>" + children + "</msub>";
         case "sup":
+            if (node.value === "2")
+                return "<mover>" + children + "</mover>";
             return "<msup>" + children + "</msup>";
+        case "su":
+            return "<msubsup>" + children + "</msupsup>";
         case "fence":
             if (node.children.length === 1) {
                 return "<mrow><mo>" + node.value.charAt(0) + "</mo>" + children + "<mo>" + node.value.charAt(1) + "</mo></mrow>";
@@ -362,8 +436,11 @@ function GetMathMLFromElement(element) {
     //   if it can’t do the conversion
     var node = GetTreeFromElement(element);
     TreePostProcessing(node);
-    console.log(node.stringify());
-    return "<math>" + TreeToMathML(node) + "</math>";
+    var mathml = TreeToMathML(node);
+    if (element.className.includes("mathcal")) {
+        mathml = "<mstyle mathvariant=script>" + mathml + "</mstyle>";
+    }
+    return "<math xmlns='http://www.w3.org/1998/Math/MathML'>" + mathml + "</math>";
 }
 
 function CreateInvisibleMathMLNode(mathmlText) {
@@ -373,8 +450,8 @@ function CreateInvisibleMathMLNode(mathmlText) {
     // The child of the span is mathml string converted to XML
     // Returns a span element
     var spanElement = document.createElement("SPAN");
-    spanElement.setAttribute("class", "mwe-math-mathml-inline mwe-math-mathml-a11y");
-    spanElement.setAttribute("style", "display: none;");
+//    spanElement.setAttribute("class", "mwe-math-mathml-inline mwe-math-mathml-a11y");
+//    spanElement.setAttribute("style", "display: none;");
     spanElement.innerHTML = mathmlText;
     return spanElement;
 }
@@ -382,6 +459,7 @@ function CreateInvisibleMathMLNode(mathmlText) {
 function FindMathElements(element) {
     if (element.className && (element.className.includes("texhtml")
             || element.className.includes("sfrac")
+            || element.className.includes("mathcal")
             || (element.nodeName === "SPAN" && element.className === "nowrap" && !element.outerHTML.includes("mathml")))) {
         var mathmlText = GetMathMLFromElement(element);
         if (mathmlText) {
